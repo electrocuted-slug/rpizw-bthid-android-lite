9534e12af6ed0581aaf93fca6ba2c921dfa33c6c
Names <names@PC-M7AE5G.lan>
2020-08-24 15:36:41 -0400

    initial commit

---
+++ bluetooth-spp-terminal/bluetooth-spp-terminal/src/main/java/ru/sash0k/bluetooth_terminal/Const.java
@@ -0,0 +1,10 @@
+package ru.sash0k.bluetooth_terminal;
+
+/**
+ * Created by sash0k on 12.12.13.
+ * Общие константы
+ */
+public class Const {
+    static final String TAG = "SPP_TERMINAL";
+    static final String KEY = "SPP_TERMINAL_KEY";
+}
